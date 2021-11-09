7631b5ec94b7271b1a18d63dc98706b4620e5f80
sebastian7722 <sebasti1001@gmail.com>
2021-11-09 17:24:36 +0100

    Přidejte soubory projektu.

---
+++ Autobazar/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Autobazar.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
